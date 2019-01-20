4333ab03d45e8014d1febc68bf4112f568454c36
Subhash Patil <patil.subhash17@gmail.com>
2019-01-20 17:21:02 +0530

    added file EXC

---
+++ Project1/Formating EXC1.py
@@ -0,0 +1,18 @@
+# x = input("enter the first no:")
+# y = input("Enter the second no")
+x,y,z = input("enter the First and second and Third no").split(",")
+# x,y,z = int(x1,y1,z1)
+# (int(x)+ int(y)+int(z))/3
+# Avg = int(x+y+z)
+print(f"Avg{(int(x)+ int(y)+int(z))/3}")
+
+# x1 = input("enter the first no:")
+# y1 = input("enter the second no:")
+# z1= input("enter the Third no:")
+#
+# x = int(x1)
+# y = int(y1)
+# z = int(z1)
+#
+# Total = (x+y+z)
+# print(f"Avarage{Total/3}")
